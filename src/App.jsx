09f01c72465d9dd7ee249937b7fffa051b4f3d28
Max-IT-ZT <babuskomax@gmail.com>
2024-11-14 02:19:12 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,40 @@
-// App.js
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Game from "./components/Game";
 import HomePage from "./components/HomePage";
 import "./App.css";
+import forestSound from "./components/sound/forest-sound.wav"; // Import the sound file
+import { GoUnmute, GoMute } from "react-icons/go"; // Import the icons
 
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
+  const audioRef = useRef(null); // Reference for the audio element
+
+  // Use useEffect to play/pause audio based on the isMuted state
+  useEffect(() => {
+    if (audioRef.current) {
+      isMuted ? audioRef.current.pause() : audioRef.current.play();
+    }
+  }, [isMuted]);
 
   const handleStart = () => {
     setIsGameStarted(true);
   };
 
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+  };
+
   return (
     <div className="App">
+      {/* Audio component with loop */}
+      <audio ref={audioRef} src={forestSound} loop />
+
+      {/* Mute/Unmute button */}
+      <button onClick={toggleMute} className="muteButton">
+        {isMuted ? <GoUnmute /> : <GoMute />}
+      </button>
+
       {isGameStarted ? <Game /> : <HomePage onStart={handleStart} />}
     </div>
   );
